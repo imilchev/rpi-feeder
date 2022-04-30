fede2266318ef6a5322d066a4c9cd3a7131d68e9
Ivan Milchev <ivan@mondoo.com>
2022-04-30 16:17:13 +0200

    added initial layout
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

---
+++ frontend/components/nav-item.tsx
@@ -0,0 +1,54 @@
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
+import { Box, Button, ListItem } from '@mui/material'
+
+interface NavItemProps {
+  href: string
+  icon: React.ReactNode
+  title: string
+}
+
+export const NavItem = (props: NavItemProps) => {
+  const router = useRouter()
+  const active = props.href ? router.pathname === props.href : false
+
+  return (
+    <ListItem
+      disableGutters
+      sx={{
+        display: 'flex',
+        mb: 0.5,
+        py: 0,
+        px: 2,
+      }}
+      // {...others}
+    >
+      <NextLink href={props.href} passHref>
+        <Button
+          component="a"
+          startIcon={props.icon}
+          disableRipple
+          sx={{
+            backgroundColor: active ? 'rgba(255,255,255, 0.08)' : undefined,
+            borderRadius: 1,
+            color: active ? 'secondary.main' : 'neutral.300',
+            fontWeight: active ? 'fontWeightBold' : undefined,
+            justifyContent: 'flex-start',
+            px: 3,
+            textAlign: 'left',
+            textTransform: 'none',
+            width: '100%',
+            '& .MuiButton-startIcon': {
+              color: active ? 'secondary.main' : 'neutral.400',
+            },
+            '&:hover': {
+              backgroundColor: 'rgba(255,255,255, 0.08)',
+            },
+          }}
+        >
+          <Box sx={{ flexGrow: 1 }}>{props.title}</Box>
+        </Button>
+      </NextLink>
+    </ListItem>
+  )
+}
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,46 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import type { ReactElement, ReactNode } from 'react'
+import { CacheProvider } from '@emotion/react'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import { ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
+import { theme } from '../theme'
+import { createEmotionCache } from '../utils/create-emotion-cache'
+import { CssBaseline } from '@mui/material'
+import { EmotionCache } from '@emotion/cache'
+import type { NextPage } from 'next'
+const clientSideEmotionCache = createEmotionCache()
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  pageProps.emotionCache = clientSideEmotionCache
+  //const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return (
+    <CacheProvider value={pageProps.emotionCache}>
+      <Head>
+        <title>Material Kit Pro</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {getLayout(<Component {...pageProps.pageProps} />)}
+        </ThemeProvider>
+      </LocalizationProvider>
+    </CacheProvider>
+  )
+}
+
+export default App
